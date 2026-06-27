9e657e8579bde60c59472dc3cc53e8cb4e811108
MassiveMassimo <mhmmadjid@gmail.com>
2026-06-27 23:07:49 +0700

    feat(match): public barrel export for the engine

---
+++ src/match/index.ts
@@ -0,0 +1,6 @@
+export { bucketKey, buildIndex } from "./build";
+export type { MatchResult } from "./match";
+export { K, match } from "./match";
+export { normalize } from "./normalize";
+export { parseRecords, serializeRecords } from "./serialize";
+export type { IndexEntry, MatchIndex, WomanRecord } from "./types";
